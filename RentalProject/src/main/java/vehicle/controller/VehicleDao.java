ef2053c8881a24f309e8ceafc799a493dcfb52cd
youngjae0216 <dudwo4213@naver.com>
2023-04-09 20:16:19 +0900

    check commit

--- RentalProject/src/main/java/vehicle/controller/VehicleDao.java
+++ RentalProject/src/main/java/vehicle/controller/VehicleDao.java
@@ -10,41 +10,41 @@ import java.util.ArrayList;
 import util.DBManager;
 import vehicle.Vehicle;
 
-
 public class VehicleDao {
 	private Connection conn;
 	private PreparedStatement pstmt;
 	private ResultSet rs;
-	
-	private VehicleDao() {}
+
+	private VehicleDao() {
+	}
+
 	private static VehicleDao instance = new VehicleDao();
+
 	public static VehicleDao getInstance() {
 		return instance;
 	}
-	
-	//C
-	
-	
-	
-	//R
-	public ArrayList<Vehicle> getVehicleAll(){
+
+	// C
+
+	// R
+	public ArrayList<Vehicle> getVehicleAll() {
 		ArrayList<Vehicle> list = new ArrayList<>();
 		this.conn = DBManager.getConnection();
-		if(conn != null) {
+		if (conn != null) {
 			String sql = "SELECT * FROM vehicle";
-			
+
 			try {
 				this.pstmt = conn.prepareStatement(sql);
 				this.rs = pstmt.executeQuery();
-				
-				while(this.rs.next()) {
+
+				while (this.rs.next()) {
 					String vehicleId = this.rs.getString(1);
 					String name = this.rs.getString(2);
 					String venueId = this.rs.getString(3);
 					Timestamp date = this.rs.getTimestamp(4);
 					int checkRev = this.rs.getInt(5);
-					
-					Vehicle vehicle = new Vehicle(vehicleId,name,venueId,date,checkRev);
+
+					Vehicle vehicle = new Vehicle(vehicleId, name, venueId, date, checkRev);
 					list.add(vehicle);
 				}
 			} catch (SQLException e) {
@@ -54,33 +54,31 @@ public class VehicleDao {
 				DBManager.close(conn, pstmt, rs);
 			}
 		}
-		
-		
-		
+
 		return list;
 	}
-	
-	public ArrayList<Vehicle> getVehicleNoneReservation(){
+
+	public ArrayList<Vehicle> getVehicleNoneReservation() {
 		ArrayList<Vehicle> list = new ArrayList<>();
 		this.conn = DBManager.getConnection();
-		if(conn != null) {
+		if (conn != null) {
 			String sql = "SELECT * FROM vehicle WHERE check_rev='0'";
-			
+
 			try {
 				this.pstmt = conn.prepareStatement(sql);
 				this.rs = pstmt.executeQuery();
-				
-				while(this.rs.next()) {
+
+				while (this.rs.next()) {
 					String vehicleId = this.rs.getString(1);
 					String name = this.rs.getString(2);
 					String venueId = this.rs.getString(3);
 					Timestamp date = this.rs.getTimestamp(4);
 					int checkRev = this.rs.getInt(5);
-					
-					Vehicle vehicle = new Vehicle(vehicleId,name,venueId,date,checkRev);
+
+					Vehicle vehicle = new Vehicle(vehicleId, name, venueId, date, checkRev);
 					list.add(vehicle);
 				}
-				
+
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -90,26 +88,25 @@ public class VehicleDao {
 		}
 		return list;
 	}
-	
+
 	public Vehicle getVehicleByName(String name) {
 		Vehicle vehicle = null;
 		this.conn = DBManager.getConnection();
-		if(conn != null) {
+		if (conn != null) {
 			String sql = "SELECT * FROM vehicle WHERE name=?";
-			
+
 			try {
 				this.pstmt = conn.prepareStatement(sql);
 				this.rs = pstmt.executeQuery();
-				while(this.rs.next()) {
+				while (this.rs.next()) {
 					String vehicleId = this.rs.getString(1);
 					String venueId = this.rs.getString(3);
 					Timestamp date = this.rs.getTimestamp(4);
 					int checkRev = this.rs.getInt(5);
-					
-					vehicle = new Vehicle(vehicleId,name,venueId,date,checkRev);
+
+					vehicle = new Vehicle(vehicleId, name, venueId, date, checkRev);
 				}
-				
-				
+
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -117,24 +114,22 @@ public class VehicleDao {
 				DBManager.close(conn, pstmt, rs);
 			}
 		}
-		
-		
+
 		return vehicle;
 	}
-	
-	
-	//U
-	
+
+	// U
+
 	public void updateVehicleRev(String vehicleId) {
 		this.conn = DBManager.getConnection();
-		if(this.conn != null) {
+		if (this.conn != null) {
 			String sql = "UPDATE vehicle SET check_rev=0 WHERE vehicle_id='?'";
-			
+
 			try {
 				this.pstmt = this.conn.prepareStatement(sql);
-				
+
 				this.pstmt.setString(1, vehicleId);
-				
+
 				this.pstmt.execute();
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
@@ -144,11 +139,7 @@ public class VehicleDao {
 			}
 		}
 	}
-	
-	
-	
-	//D
-	
-	
-	
+
+	// D
+
 }
